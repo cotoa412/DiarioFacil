dfc61cf6c76a4f4f60b583c92b7a8b54aa409b29
ANTHONY ESTIVEN FLORES BOZA <anthony.flores@ulatina.net>
2019-06-11 19:36:57 -0600

    Agregué la clase Usuario

---
+++ src/com/ulatina/diarioFacil/Usuario.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ulatina.diarioFacil;
+
+/**
+ *
+ * @author Personal
+ */
+public abstract class Usuario {
+    
+    private String nombreUsuario = "";
+    private String contrasenna = "";
+
+    public Usuario() {
+        
+    }
+    
+    public Usuario(String nombreUsuario,String contrasenna) {
+        
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenna = contrasenna;
+        
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenna() {
+        return contrasenna;
+    }
+
+    public void setContrasenna(String contrasenna) {
+        this.contrasenna = contrasenna;
+    }
+    
+    public String toString(){
+    
+        return this.getNombreUsuario() + " " + this.getContrasenna();
+        
+    }
+    
+}
